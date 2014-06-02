da0ce2647dde9e7d6206c6437db8d82eaf66ac04
David Amian <damian@emergya.com>
2014-06-02 18:20:58 +0200

    Added configuration

---
+++ gcs/newfiles_skel/opt/firefox/browser/defaults/preferences/scope.js
@@ -0,0 +1,6 @@
+// cosa
+pref("extensions.autoDisableScopes",0);
+pref("extensions.enabledScopes", 15);
+pref("app.update.auto", false);
+pref("app.update.channel", "");
+pref("browser.shell.checkDefaultBrowser", false);
